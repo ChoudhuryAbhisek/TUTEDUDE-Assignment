fafcf299f050a99dc4933f265e74c9d7d3e10a16
ChoudhuryAbhisek <rohitabhisekchoudhury2001@gmail.com>
2025-06-01 21:25:47 +0530

    Add files via upload

---
+++ Assignment_2_Task_1.py
@@ -0,0 +1,3 @@
+num=int(input("Enter a number: "))
+
+def factorial(num):
---
+++ Assignment_3_Task_1.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+def factorial(num):
+  if num<=1:
+    return 1
+  else:
+    return num*factorial(num-1)
+
+print("Factorial of", n, "is:",factorial(n))
